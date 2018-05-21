6f87717fe056777b7a19c43a2c8841bacee34ebf
David Bernado <dbernado@protonmail.com>
2018-05-21 10:31:56 -0700

    optimizations and bug fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,101 +1,99 @@
 extern crate regex;
 
-use std::fs;
-use std::env;
-use std::error::Error;
-use self::regex::Regex;
-use self::regex::escape;
-use std::process;
+pub mod grep {
+    use std::fs;
+    use std::env;
+    use std::error::Error;
+    use ::regex::Regex;
+    use ::regex::escape;
+    use std::process;
 
-pub fn grep_exec(query: &str, filename: &str, flag: &str) {
-    let contents = read_file(&filename).unwrap_or_else(|err| {
-        eprintln!("Problem reading file: {}", err);
-        process::exit(1);
-    });
+    pub fn grep_exec(query: &str, filename: &str, flag: &str) {
+        let contents = read_file(&filename).unwrap_or_else(|err| {
+            eprintln!("Problem reading file: {}", err);
+            process::exit(1);
+        });
 
-    println!("'{}' occurs {} times in {}", query, search(&query, &contents, &flag), filename);
-}
+        println!("'{}' occurs {} times in {}", query, search(&query, &contents, &flag), filename);
+    }
 
-pub fn read_file(filename: &str) -> Result<String, Box<Error>> {
-    let contents = fs::read_to_string(filename)?;
+    pub fn read_file(filename: &str) -> Result<String, Box<Error>> {
+        let contents = fs::read_to_string(filename)?;
 
-    Ok(contents)
-}
+        Ok(contents)
+    }
 
-pub fn search(query: &str, contents: &str, flag: &str) -> usize {
-    let re = Regex::new(format!(r#"{}\b"#, query.to_lowercase().as_str()).as_str()).unwrap();
-    let mut score = 0;
+    pub fn search(query: &str, contents: &str, flag: &str) -> usize {
+        let re = Regex::new(format!(r#"{}\b"#, query.to_lowercase().as_str()).as_str()).unwrap();
 
-    let case_sensitive = String::from("-c");
-    let none = String::from("NO_FLAG");
+        let case_sensitive = String::from("-c");
+        let none = String::from("NO_FLAG");
 
-    match flag {
-        case_sensitive => {
-            let lowercased = contents.to_lowercase();
-            let text = escape(lowercased.as_str());
-
-            for _mat in re.find_iter(text.as_str()) {
-                score = score + 1;
+        match flag {
+            case_sensitive => {
+                let lowercased = contents.to_lowercase();
+                let text = escape(lowercased.as_str());
+                let score = re.find_iter(text.as_str()).count();
+                
+                score
             }
-        }
-        none => {
-            let text = escape(contents);
-            for _mat in re.find_iter(text.as_str()) {
-                score = score + 1;
+            none => {
+                let text = escape(contents);
+                let score = re.find_iter(text.as_str()).count();
+
+                score
             }
         }
     }
 
-    score
-}
+    pub struct Config {
+        pub query: String,
+        pub filename: String,
+        pub flag: String,
+    }
 
-pub struct Config {
-    pub query: String,
-    pub filename: String,
-    pub flag: String,
-}
+    impl Config {
+        pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+            args.next();
 
-impl Config {
-    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-        args.next();
+            let args_length = args.len();
+            match args_length {
+                1 => Err("Not enough arguments"),
+                2 => {
+                    let query = match args.next() {
+                        Some(arg) => arg,
+                        None => return Err("Didn't get a query string"),
+                    };
 
-        let args_length = args.len();
-        match args_length {
-            1 => Err("Not enough arguments"),
-            2 => {
-                let query = match args.next() {
-                    Some(arg) => arg,
-                    None => return Err("Didn't get a query string"),
-                };
+                    let filename = match args.next() {
+                        Some(arg) => arg,
+                        None => return Err("Didn't get a filename"),
+                    };
 
-                let filename = match args.next() {
-                    Some(arg) => arg,
-                    None => return Err("Didn't get a filename"),
-                };
+                    let flag = String::from("NO_FLAG");
 
-                let flag = String::from("NO_FLAG");
+                    Ok(Config {query, filename, flag})
+                },
+                3 => {
+                    let query = match args.next() {
+                        Some(arg) => arg,
+                        None => return Err("Didn't get a query string"),
+                    };
 
-                Ok(Config {query, filename, flag})
-            },
-            3 => {
-                let query = match args.next() {
-                    Some(arg) => arg,
-                    None => return Err("Didn't get a query string"),
-                };
-
-                let filename = match args.next() {
-                    Some(arg) => arg,
-                    None => return Err("Didn't get a filename"),
-                };
+                    let filename = match args.next() {
+                        Some(arg) => arg,
+                        None => return Err("Didn't get a filename"),
+                    };
                 
-                let flag = match args.next() {
-                    Some(arg) => arg,
-                    None => return Err("Didn't get a flag"),
-                };
+                    let flag = match args.next() {
+                        Some(arg) => arg,
+                        None => return Err("Didn't get a flag"),
+                    };
 
-                Ok(Config {query, filename, flag})
-            },
-            _ => Err("Too many arguments"),
+                    Ok(Config {query, filename, flag})
+                },
+                _ => Err("Too many arguments"),
+            }
         }
     }
 }
